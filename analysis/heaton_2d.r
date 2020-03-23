49cb418d72d56902bdbd2c1715fde04c2c895ea7
mkln <peruzzi@gmail.com>
2020-03-23 01:37:12 +0100

    reproduce this

--- analysis/heaton_2d.r
+++ analysis/heaton_2d.r
@@ -11,107 +11,112 @@ library(meshgp)
 
 set.seed(ss <- 1)
 
-simdata <- F
-if(simdata){
-  #load("data/AllSimulatedTemps.RData")
-  load(url("https://github.com/finnlindgren/heatoncomparison/raw/master/Data/AllSimulatedTemps.RData"))
-  dfname <- all.sim.data 
-  label <- "simulated"
-} else {
-  #load("data/AllSatelliteTemps.RData")
-  load(url("https://github.com/finnlindgren/heatoncomparison/raw/master/Data/AllSatelliteTemps.RData"))
-  dfname <- all.sat.temps
-  label <- "realdata"
+simdata <- T
+for(simdata in c(F, T)){
+  for(Mv in list(c(25, 15), c(50, 30), c(100, 60))){
+    if(simdata){
+      #load("data/AllSimulatedTemps.RData")
+      load(url("https://github.com/finnlindgren/heatoncomparison/raw/master/Data/AllSimulatedTemps.RData"))
+      dfname <- all.sim.data 
+      label <- "simulated"
+    } else {
+      #load("data/AllSatelliteTemps.RData")
+      load(url("https://github.com/finnlindgren/heatoncomparison/raw/master/Data/AllSatelliteTemps.RData"))
+      dfname <- all.sat.temps
+      label <- "realdata"
+    }
+    
+    mapcenter <- dfname %>% select(Lon, Lat) %>% apply(2, mean)
+    mapcenter_rpmt <- matrix(1, nrow=nrow(dfname), ncol=1) %x% t(mapcenter)
+    rangeLon <- diff(range(dfname$Lon))
+    
+    simdf <- dfname %>% 
+      rename(Var1 = Lon, Var2 = Lat,
+             y=MaskTemp, y_full=TrueTemp) %>% arrange(Var1, Var2)
+    
+    coords <- simdf %>% select(Var1, Var2) %>% as.matrix() %>% 
+      add(-mapcenter_rpmt) %>% 
+      multiply_by(1/rangeLon) %>% 
+      add(.5)
+    
+    nr <- nrow(coords)
+    X <- cbind(coords)#
+    Z <- matrix(1, nrow=nr)
+    ybar <- mean(simdf$y, na.rm=T)
+    y <- simdf$y - ybar
+    
+    
+    mcmc_keep <- 1000
+    mcmc_burn <- 4000
+    mcmc_thin <- 2
+    
+    mesh_mcmc <- list(keep=mcmc_keep, burn=mcmc_burn, thin=mcmc_thin)
+    mesh_settings  <-  list(adapting=T, mcmcsd=.3, cache=T, cache_gibbs=F, 
+                            reference_full_coverage=F, 
+                            verbose=F, debug=F, 
+                            printall=T, saving=F)
+    mesh_debug <- list(sample_beta=T, sample_tausq=T, sample_sigmasq=T, sample_theta=T, sample_w=T)
+    
+    # MESH
+    coords %>% apply(2, function(x) x %>% unique %>% length)
+    #Mv <- c(100,60) # 
+    (nr / prod(Mv) * ncol(Z))
+    
+    set.seed(1)
+    mesh_time <- system.time({
+      meshout <- meshgp(y, X, Z, coords, Mv=Mv, 
+                        mcmc = mesh_mcmc,
+                        settings = mesh_settings,
+                        num_threads = 11)
+    }) 
+    
+    beta_mcmc <- meshout$beta_mcmc
+    sigmasq_mcmc <- meshout$sigmasq_mcmc
+    tausq_mcmc <- meshout$tausq_mcmc
+    theta_mcmc <- meshout$theta_mcmc
+    w_mcmc <- meshout$w_mcmc
+    yhat_mcmc <- meshout$yhat_mcmc
+    
+    (beta_est <- beta_mcmc %>% apply(1, mean))
+    (sigmasq_est <- sigmasq_mcmc %>% mean())
+    (tausq_est <- tausq_mcmc %>% mean())
+    (theta_est <- theta_mcmc %>% apply(1, mean))
+    
+    ybar <- 0
+    meshgp_df <- meshout$coords %>% 
+      cbind(w_mcmc    %>% list_mean(),
+            yhat_mcmc %>% list_mean() %>% add(ybar),
+            yhat_mcmc %>% list_qtile(0.025) %>% add(ybar),
+            yhat_mcmc %>% list_qtile(.975) %>% add(ybar)) %>%
+      as.data.frame() %>% 
+      arrange(Var1, Var2) 
+    
+    colnames(meshgp_df) <- c("Var1", "Var2", paste0("w_meshgp_", 1:ncol(Z)), 
+                             "y_meshgp", "y_meshgp_low", "y_meshgp_hi")
+    
+    
+    model_compare <- coords %>% cbind(simdf$y_full, y) %>% as.data.frame() %>%
+      rename(y_full = V3) %>% left_join(meshgp_df) %>%
+      arrange(Var1, Var2) 
+    
+    outsample <- model_compare %>% filter(!complete.cases(y)) 
+    outsample %$% sqrt(mean((y_meshgp-y_full)^2, na.rm=T))
+    outsample %$% mean(abs(y_meshgp-y_full), na.rm=T)
+    outsample %$% mean((y_meshgp_low<y_full) & (y_full<y_meshgp_hi), na.rm=T)
+    
+    #> outsample %$% sqrt(mean((y_meshgp-y_full)^2, na.rm=T))
+    #[1] 1.58725
+    #> outsample %$% mean(abs(y_meshgp-y_full), na.rm=T)
+    #[1] 1.133814
+    #> outsample %$% mean((y_meshgp_low<y_full) & (y_full<y_meshgp_hi), na.rm=T)
+    #[1] 0.9291764
+    
+    save(file="postprocess/heaton/heaton_{label}_perf_{Mv[1]}x{Mv[2]}_I_noybar.RData" %>% glue::glue(), 
+         list=c("outsample", "Mv", "model_compare", "mesh_mcmc", "mesh_settings", "mesh_debug",
+                "meshout", "mesh_time", "meshgp_df"))
+    
+  }
 }
-
-mapcenter <- dfname %>% select(Lon, Lat) %>% apply(2, mean)
-mapcenter_rpmt <- matrix(1, nrow=nrow(dfname), ncol=1) %x% t(mapcenter)
-rangeLon <- diff(range(dfname$Lon))
-
-simdf <- dfname %>% 
-  rename(Var1 = Lon, Var2 = Lat,
-         y=MaskTemp, y_full=TrueTemp) %>% arrange(Var1, Var2)
-
-coords <- simdf %>% select(Var1, Var2) %>% as.matrix() %>% 
-  add(-mapcenter_rpmt) %>% 
-  multiply_by(1/rangeLon) %>% 
-  add(.5)
-
-nr <- nrow(coords)
-X <- cbind(coords)#
-Z <- matrix(1, nrow=nr)
-ybar <- mean(simdf$y, na.rm=T)
-y <- simdf$y# - ybar
-
-
-mcmc_keep <- 1000
-mcmc_burn <- 3000
-mcmc_thin <- 2
-
-mesh_mcmc <- list(keep=mcmc_keep, burn=mcmc_burn, thin=mcmc_thin)
-mesh_settings  <-  list(adapting=T, mcmcsd=.3, cache=T, cache_gibbs=F, 
-                        reference_full_coverage=F, 
-                        verbose=F, debug=F, 
-                        printall=T, saving=F)
-mesh_debug <- list(sample_beta=T, sample_tausq=T, sample_sigmasq=T, sample_theta=T, sample_w=T)
-
-# MESH
-coords %>% apply(2, function(x) x %>% unique %>% length)
-Mv <- c(100,60) # 
-(nr / prod(Mv) * ncol(Z))
-
-set.seed(1)
-mesh_time <- system.time({
-  meshout <- meshgp(y, X, Z, coords, Mv=Mv, 
-                    mcmc = mesh_mcmc,
-                    settings = mesh_settings,
-                    num_threads = 11)
-}) 
-
-beta_mcmc <- meshout$beta_mcmc
-sigmasq_mcmc <- meshout$sigmasq_mcmc
-tausq_mcmc <- meshout$tausq_mcmc
-theta_mcmc <- meshout$theta_mcmc
-w_mcmc <- meshout$w_mcmc
-yhat_mcmc <- meshout$yhat_mcmc
-
-(beta_est <- beta_mcmc %>% apply(1, mean))
-(sigmasq_est <- sigmasq_mcmc %>% mean())
-(tausq_est <- tausq_mcmc %>% mean())
-(theta_est <- theta_mcmc %>% apply(1, mean))
-
-ybar <- 0
-meshgp_df <- meshout$coords %>% 
-  cbind(w_mcmc    %>% list_mean(),
-        yhat_mcmc %>% list_mean() %>% add(ybar),
-        yhat_mcmc %>% list_qtile(0.025) %>% add(ybar),
-        yhat_mcmc %>% list_qtile(.975) %>% add(ybar)) %>%
-  as.data.frame() %>% 
-  arrange(Var1, Var2) 
-
-colnames(meshgp_df) <- c("Var1", "Var2", paste0("w_meshgp_", 1:ncol(Z)), 
-                         "y_meshgp", "y_meshgp_low", "y_meshgp_hi")
-
-
-model_compare <- coords %>% cbind(simdf$y_full, y) %>% as.data.frame() %>%
-  rename(y_full = V3) %>% left_join(meshgp_df) %>%
-  arrange(Var1, Var2) 
-
-outsample <- model_compare %>% filter(!complete.cases(y)) 
-outsample %$% sqrt(mean((y_meshgp-y_full)^2, na.rm=T))
-outsample %$% mean(abs(y_meshgp-y_full), na.rm=T)
-outsample %$% mean((y_meshgp_low<y_full) & (y_full<y_meshgp_hi), na.rm=T)
-
-#> outsample %$% sqrt(mean((y_meshgp-y_full)^2, na.rm=T))
-#[1] 1.58725
-#> outsample %$% mean(abs(y_meshgp-y_full), na.rm=T)
-#[1] 1.133814
-#> outsample %$% mean((y_meshgp_low<y_full) & (y_full<y_meshgp_hi), na.rm=T)
-#[1] 0.9291764
-
-save(file="postprocess/heaton_{label}_perf_{Mv[1]}x{Mv[2]}.RData" %>% glue::glue(), 
-     list=c("outsample", "Mv", "model_compare", "mesh_mcmc", "mesh_settings", "mesh_debug",
-            "meshout", "mesh_time", "meshgp_df"))
 
 ##################
 # POSTPROCESSING #
---
+++ analysis/simsetup_3d.r
@@ -0,0 +1,199 @@
+rm(list=ls())
+library(tidyverse)
+library(magrittr)
+library(meshgp)
+library(gapfill)
+
+set.seed(1) 
+# use powers of two to make layering easy for now.
+dd <- 3 # dimension
+SS <- 40 # coord values for jth dimension 
+TT <- 10
+nr <- SS^2 * TT # tot obs
+
+xlocs <- seq(0.0, 1, length.out=SS+2) %>% head(-1) %>% tail(-1)
+tlocs <- seq(0.0, 1, length.out=TT+2) %>% head(-1) %>% tail(-1)
+coords <- expand.grid(xlocs, xlocs, tlocs) %>% arrange(Var1, Var2, Var3)
+cx <- coords %>% as.matrix()
+uni_time <- unique(coords$Var3)
+
+dnr <- coords$Var1 %>% unique() %>% length()
+dnc <- coords$Var2 %>% unique() %>% length()
+dns <- coords$Var3 %>% unique() %>% length()
+
+
+# data gen
+## parameter grid
+sigmasq_vals <- 1
+tausq_vals <- c(.001, .05, .1)
+a_vals <- c(5, 50, 500) # time a
+beta_vals <- c(0.05, 0.5, .95) # separability
+c_vals <- c(1, 5, 25) # space c
+
+pars_grid <- expand.grid(sigmasq_vals, tausq_vals, a_vals, beta_vals, c_vals)
+colnames(pars_grid) <- c("sigmasq", "tausq", "a_vals", "beta_vals", "c_vals")
+results <- pars_grid
+results %<>% mutate(mesh_abs = NA,
+                    mesh_rmse = NA,
+                    mesh_covg = NA,
+                    gf_abs = NA,
+                    gf_rmse = NA,
+                    gf_covg = NA)
+
+for(s in 1:nrow(pars_grid)){
+  
+  cat("Simulation", s, '\n')
+  
+  X <- matrix(1, nrow=nr)
+  colnames(X) <- "X_1"
+  Z <- matrix(1, nrow=nr)
+  colnames(Z) <- "Z_1"
+  p <- 1
+  
+  
+  sigma.sq <- pars_grid[s, "sigmasq"]
+  tau.sq <- pars_grid[s, "tausq"]
+  aa <- pars_grid[s, "a_vals"]
+  bb <- pars_grid[s, "beta_vals"]
+  cc <- pars_grid[s, "c_vals"]
+  Dmat <- matrix(0, ncol=1)
+  
+  print(pars_grid[s,])
+  
+  cxi <- 1:nrow(cx)-1
+  
+  #w <- meshgp::rqmeshgp(cx, c(10,10,5), c(sigma.sq, aa, bb, cc), Dmat)
+  
+  
+  R123 <- xCovHUV(cx, cxi, cxi, c(sigma.sq, aa, bb, cc), Dmat, T)#,
+  w <- meshgp::mvn(1, rep(0,nr), R123) %>% t()
+
+  
+  y_full <- w + sqrt(tau.sq) * rnorm(nr) 
+  
+  # make some na: 0=na
+  na_which <- 1:nr 
+  full_times <- uni_time %>% sample(2)
+  missing_times <- uni_time %>% setdiff(full_times) %>% sample(2)
+  for(mt in missing_times){
+    selector <- coords$Var3 %in% mt
+    na_which[selector] <- NA
+    save_some <- which(selector) %>% sample(10)
+    na_which[save_some] <- 1
+  }
+  
+  nonmissing_times <- uni_time %>% setdiff(full_times) %>% setdiff(missing_times)
+  for(nmt in nonmissing_times){
+    circle_center <- runif(2, 0, 1)
+    
+    missing_circle <- ((coords$Var1-circle_center[1])^2 + 
+                         (coords$Var2-circle_center[2])^2 < .05) & 
+      (coords$Var3 %in% nmt)
+    na_which[missing_circle] <- NA
+  }
+  
+  
+  y <- y_full * na_which#[!is.na(na_which)]# 
+  
+  p <- ncol(X)
+  
+  
+  #########
+  # Q-MGP #
+  #########
+  
+  mcmc_keep <- 1000
+  mcmc_burn <- 5000
+  mcmc_thin <- 2
+  
+  set.seed(1)
+  mesh_time <- system.time({
+    test <- meshgp(y, X, Z, coords, Mv=c(10, 10, 5), 
+                   mcmc = list(keep=mcmc_keep, burn=mcmc_burn, thin=mcmc_thin),
+                   num_threads = 11,
+                   debug       = list(sample_beta=F, sample_tausq=T, sample_sigmasq=T, sample_theta=T, sample_w=T))
+  })
+  
+  (beta_est <- test$beta_mcmc %>% apply(1, mean))
+  (sigmasq_est <- test$sigmasq_mcmc %>% mean())
+  (tausq_est <- test$tausq_mcmc %>% mean())
+  (theta_est <- test$theta_mcmc %>% apply(1, mean))
+  
+  meshgp_df <- coords %>% 
+    cbind("y_meshgp" = test$yhat_mcmc %>% list_mean(),
+          "y_meshgp_lo" = test$yhat_mcmc %>% list_qtile(.05),
+          "y_meshgp_hi" = test$yhat_mcmc %>% list_qtile(.95)) %>%
+    as.data.frame() %>% 
+    arrange(Var1, Var2, Var3) 
+  
+  ###########
+  # Gapfill #
+  ###########
+  
+  dfc <- coords %>% cbind(y)
+  
+  darray <- dfc %>% 
+    split(dfc$Var3) %>% 
+    lapply(function(x) x %>% select(-Var3) %>% spread(Var2, y) %>% select(-Var1) %>% as.matrix()) %>%
+    abind::abind(along=3)
+  
+  gfarray <- array(0, dim=c(dnr, dnc, 1, dns))
+  gfarray[,,1,] <- darray
+  
+  
+  gapfill_time <- system.time({
+    filled_all <- Gapfill(gfarray, verbose=T, 
+                          dopar = F,
+                          nPredict = 3, predictionInterval = TRUE) %$% fill 
+    filled <- filled_all[,,1,,1]
+    ciLo <- filled_all[,,1,,2]
+    ciUp <- filled_all[,,1,,3]
+  })
+  
+  filled_long <- filled %>% 
+    plyr::alply(3, function(x) reshape2::melt(x)) %>%
+    bind_rows()
+  Lo_long <- ciLo %>% 
+    plyr::alply(3, function(x) reshape2::melt(x)) %>%
+    bind_rows()
+  Up_long <- ciUp %>% 
+    plyr::alply(3, function(x) reshape2::melt(x)) %>%
+    bind_rows()
+  gfresults <- coords %>% as.data.frame() %>% 
+    arrange(Var3, Var2, Var1) %>% 
+    cbind(filled_long$value, Lo_long$value, Up_long$value) %>% as.data.frame()
+  colnames(gfresults) <- c("Var1", "Var2", "Var3", "y_gf", "y_gf_lo", "y_gf_hi")
+  
+  
+  ###########
+  # Results #
+  ###########
+  
+  analysis_plot_df <- coords %>% cbind(y, y_full) %>% rename(y_miss=y, y=y_full) %>%
+    left_join(meshgp_df) %>% left_join(gfresults) %>%
+    mutate(mesh_inrange = (y_meshgp_lo<y) & (y<y_meshgp_hi),
+           gf_inrange = (y_gf_lo<y) & (y<y_gf_hi))
+  
+  analysis_plot_df %>% filter(!complete.cases(y_miss)) %>% 
+    select(y, y_meshgp) %>% as.matrix() %>% cor()
+  
+  results[s, "mesh_rmse"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% sqrt(mean((y-y_meshgp)^2))
+  results[s, "gf_rmse"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% sqrt(mean((y-y_gf)^2))
+  
+  results[s, "mesh_abs"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% mean(abs(y-y_meshgp))
+  results[s, "gf_abs"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% mean(abs(y-y_gf))
+  
+  results[s, "mesh_covg"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% mean(mesh_inrange)
+  results[s, "gf_covg"] <- analysis_plot_df %>% filter(!complete.cases(y_miss)) %$% mean(gf_inrange)
+  
+  print(results[1:s,])
+}
+
+#save(file="postprocess/mesh_vs_gapfill_gp_3d_nonsep_onlyclouds.RData", list=c("results"))
+
+# sample plot
+analysis_plot_df %>% filter(Var3==unique(Var3)[6]) %>% 
+  select(-Var3, -contains("inrange"), -contains("_hi"), -contains("_lo")) %>% plotspatial()
+
+
+
